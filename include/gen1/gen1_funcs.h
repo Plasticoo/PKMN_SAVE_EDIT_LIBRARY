e414f2667c65df8f6c04f32d70fbfa19d1785185
Romeu Gomes <oh4ianon@hotmail.com>
2017-02-05 18:19:57 +0000

    Gen1 Checksum function, no idea if its working.

---
+++ include/gen1/gen1_funcs.h
@@ -0,0 +1,10 @@
+#ifndef __GEN1_FUNCS_H_
+#define __GEN1_FUNCS_H_
+
+#include "gen1_structs.h"
+
+#include <stdio.h>
+
+uint8_t gen1_checksum(FILE* file_name);
+
+#endif
---
+++ lib/gen1/gen1_funcs.c
@@ -0,0 +1,21 @@
+#include "../../include/gen1/gen1_funcs.h"
+
+#define CHECKSUM_INIT_OFFSET 0x2598
+#define CHECKSUM_END_OFFSET  0x3522
+
+// TODO Test
+uint8_t gen1_checksum(FILE* file_name) {
+    int i;
+
+    uint8_t checksum = 0;
+    uint8_t byte;
+
+    fseek(file_name, CHECKSUM_INIT_OFFSET, SEEK_SET);
+
+    for (i = 0; i < CHECKSUM_END_OFFSET - CHECKSUM_INIT_OFFSET; i++) {
+        fread(&byte, 1, 1, file_name);
+        checksum += byte;
+    }
+
+    return ~checksum;
+}
