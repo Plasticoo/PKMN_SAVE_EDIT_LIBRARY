5ef5db34aa9270442036ca4030ffcd7a14930ee3
Romeu Gomes <oh4ianon@hotmail.com>
2017-02-07 16:42:25 +0000

    Use compiler warnings...

--- tests/compile.sh
+++ tests/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc checksum_test.c ../lib/gen1/gen1_funcs.c -o checksum_test
-gcc file_load_test.c ../lib/gen1/gen1_funcs.c ../lib/gen1/gen1_prints.c -o file_load_test
+gcc -Wall checksum_test.c ../lib/gen1/gen1_funcs.c -o checksum_test
+gcc -Wall file_load_test.c ../lib/gen1/gen1_funcs.c ../lib/gen1/gen1_prints.c -o file_load_test
