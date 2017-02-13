a582e1e57b3bbfca4aaa99e982f28b1c1e6fe0c4
Romeu Gomes <oh4ianon@hotmail.com>
2017-02-13 23:47:56 +0000

    Created functions to do conversions between BCD and hex.

---
+++ include/gen1/gen1_math.h
@@ -0,0 +1,9 @@
+#ifndef __GEN1_MATH_H_
+#define __GEN1_MATH_H_
+
+#include <inttypes.h>
+
+uint8_t __bcd_to_dec(uint8_t _bcd);
+uint8_t __dec_to_bcd(uint8_t _dec);
+
+#endif
---
+++ lib/gen1/gen1_math.c
@@ -0,0 +1,9 @@
+#include <inttypes.h>
+
+uint8_t __bcd_to_dec(uint8_t _bcd) {
+    return (((_bcd & 0xF0) >> 4) * 10) + (_bcd & 0x0F);
+}
+
+uint8_t __dec_to_bcd(uint8_t _dec) {
+    return ((_dec / 10) << 4) | (_dec % 10);
+}
