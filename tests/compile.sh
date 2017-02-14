625c1983178bb25dae72707d616f866c5fa5e454
Romeu Gomes <oh4ianon@hotmail.com>
2017-02-14 01:11:58 +0000

    Add tests for integer concatenation, just to make sure.

--- tests/compile.sh
+++ tests/compile.sh
@@ -2,3 +2,4 @@
 
 gcc -Wall -ggdb checksum_test.c ../lib/gen1/gen1_funcs.c -o checksum_test
 gcc -Wall -ggdb file_load_test.c ../lib/gen1/gen1_structs.c ../lib/gen1/gen1_funcs.c ../lib/gen1/gen1_prints.c -o file_load_test
+gcc -Wall -ggdb number_concat_test.c ../lib/gen1/gen1_math.c -o number_concat_test
