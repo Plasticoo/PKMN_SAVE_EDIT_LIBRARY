3fcc2bf681255712aaf45390c3474882ea8d0359
Romeu Gomes <oh4ianon@hotmail.com>
2017-03-10 23:04:45 +0000

    Implementing a way of printing debug messages.

--- include/gen1/gen1_defines.h
+++ include/gen1/gen1_defines.h
@@ -96,4 +96,14 @@
 #define uint32_t u32
 #define uint64_t u64
 
+#ifdef DEBUG
+#define DEBUG_TEST 1
+#else
+#define DEBUG_TEST 0
+#endif
+
+#define PDEBUG(fmt, ...)                                           \
+    do { if (DEBUG_TEST) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__,  \
+                                 __LINE__, __func__, __VA_ARGS__); } while (0)
+
 #endif
