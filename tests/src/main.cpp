5bd3d4cb04d0948c28eaa83360f5f5abee4d32de
Romeu Vieira <romeu.bizz@gmail.com>
2019-08-17 18:39:48 +0100

    Start creating gen1 tests (WIP)

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,18 +1,72 @@
+#define CATCH_CONFIG_MAIN
+
 #include <iostream>
 #include <cstdio>
+
+#include "catch2/catch.hpp"
 
 #include "RomInterface.hpp"
 #include "Gen1.hpp"
 
-int main()
+TEST_CASE("Generation 1 class gets correct information")
 {
 	auto _rom = std::make_unique<Rom32kb>("../../games/yellow.sav");
-	std::printf("Save file size: %lu\n", _rom->get_size());
 
-	auto gen1 = new Gen1(std::move(_rom));
-	gen1->load_file();
+	SECTION("File is loaded correctly and has correct size")
+    {
+        REQUIRE(_rom->get_size() == 0x8000);
+    }
 
-	std::printf("Money: %d\n", gen1->get_money());
+    auto gen1 = new Gen1(std::move(_rom));
 
-    return 0;
+	SECTION("Player name is ROMEU")
+	{
+		REQUIRE(gen1->get_player_name() == "ROMEU");
+	}
+
+	SECTION("Rival name is GARY")
+	{
+		REQUIRE(gen1->get_rival_name() == "GARY");
+	}
+
+    // SECTION("Number in Pokedex Owned is X")
+    // {
+    //     REQUIRE(gen1->get_pokede == "GARY");
+    // }
+
+    // SECTION("Number in Pokedex Owned is Y")
+    // {
+    //     REQUIRE(gen1->get_rival_name() == "GARY");
+    // }
+
+    SECTION("Money is 3175")
+    {
+        REQUIRE(gen1->get_money() == 3175);
+    }
+
+	SECTION("Casino Coins is 0")
+    {
+        REQUIRE(gen1->get_casino_coins() == 0);
+    }
+
+	SECTION("Time played is 00:01:00")
+	{
+		auto time_played = Gen1Structs::pkmn_time();
+		gen1->get_time_played(&time_played);
+
+		SECTION("Hours is XX")
+		{
+			REQUIRE(time_played.hours == 0);
+		}
+
+		SECTION("Minutes is XX")
+		{
+			REQUIRE(time_played.minutes == 1);
+		}
+
+		SECTION("Seconds is XX")
+		{
+			REQUIRE(time_played.seconds == 0);
+		}
+	}
 }
