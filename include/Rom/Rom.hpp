d2e81aa76ab871fb80ca6185a45c41c726350921
Romeu Vieira <romeu.bizz@gmail.com>
2019-11-02 22:32:34 +0000

    Rom template

--- include/Rom/Rom.hpp
+++ include/Rom/Rom.hpp
@@ -1,21 +1,109 @@
 #ifndef _PKMN_ROM_HPP_
 #define _PKMN_ROM_HPP_
 
+#include "Constants.hpp"
 #include "RomInterface.hpp"
 
 #include <array>
 #include <cinttypes>
 #include <filesystem>
+#include <fstream>
 #include <memory>
 
-struct Rom: RomInterface {
-    std::array<std::uint8_t, 131072> data;
-    std::uint8_t type;
+struct Rom32kb{};
+struct Rom64kb{};
 
-    Rom(std::filesystem::path const& file);
+template<typename T>
+struct Rom : RomInterface {
 
-    auto load(std::filesystem::path const& file) -> void override;
-    auto get_size() const -> std::size_t override;
+	std::array<std::uint8_t, 0x8000> data;
+
+	Rom(std::filesystem::path const& file) {
+		this->load(file);
+	}
+
+    auto load(std::filesystem::path const& file) -> void override
+    {
+        std::ifstream ifs(file, std::ios::binary | std::ios::ate);
+        if (!ifs) {
+            return;
+        }
+
+        auto size = std::filesystem::file_size(file);
+        if (size == 0 || size != C::GB::ROM_SIZE) {
+            return;
+        }
+
+        if (ifs.read((char*)this->data.data(), size)) {
+            return;
+        }
+    }
+
+    auto get_size() const -> std::size_t override {
+		return this->data.size();
+	}
+};
+
+template<>
+struct Rom<Rom32kb> : RomInterface
+{
+	std::array<std::uint8_t, 0x8000> data;
+
+	Rom(std::filesystem::path const& file) {
+		this->load(file);
+	}
+
+    auto load(std::filesystem::path const& file) -> void override
+    {
+        std::ifstream ifs(file, std::ios::binary | std::ios::ate);
+        if (!ifs) {
+            return;
+        }
+
+        auto size = std::filesystem::file_size(file);
+        if (size == 0 || size != C::GB::ROM_SIZE) {
+            return;
+        }
+
+        if (ifs.read((char*)this->data.data(), size)) {
+            return;
+        }
+    }
+
+    auto get_size() const -> std::size_t override {
+		return this->data.size();
+	}
+};
+
+template<>
+struct Rom<Rom64kb> : RomInterface
+{
+	std::array<std::uint8_t, 0x10000> data;
+
+	Rom(std::filesystem::path const& file) {
+		this->load(file);
+	}
+
+    auto load(std::filesystem::path const& file) -> void override
+    {
+        std::ifstream ifs(file, std::ios::binary | std::ios::ate);
+        if (!ifs) {
+            return;
+        }
+
+        auto size = std::filesystem::file_size(file);
+        if (size == 0 || size != C::GB::ROM_SIZE) {
+            return;
+        }
+
+        if (ifs.read((char*)this->data.data(), size)) {
+            return;
+        }
+    }
+
+    auto get_size() const -> std::size_t override {
+		return this->data.size();
+	}
 };
 
 #endif
